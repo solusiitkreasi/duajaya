71a342d9c328e6151a11b4e6a43ceedbb782857c
Aryatama <18211338+aryatama045@users.noreply.github.com>
2022-12-09 16:15:14 +0700

    Update main.blade.php

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -1386,20 +1386,7 @@
     @endif
     @stack('scripts')
 
-    // <script>
 
-    //     if ('serviceWorker' in navigator ) {
-    //         window.addEventListener('load', function() {
-    //             navigator.serviceWorker.register( 'pos/service-worker.js').then(function(registration) {
-    //                 // Registration was successful
-    //                 console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    //             }, function(err) {
-    //                 // registration failed :(
-    //                 console.log('ServiceWorker registration failed: ', err);
-    //             });
-    //         });
-    //     }
-    // </script>
     <script type="text/javascript">
 
       var alert_product = <?php echo json_encode($alert_product) ?>;
